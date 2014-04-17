b5967a3bcb78245d4fa9593fb528d76770cf7fef
qianfeng <apple@aofel.local>
2014-04-17 11:57:37 +0800

    Initial Commit

---
+++ NSLock_4.17/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSLock_4.17
+//
+//  Created by qianfeng on 14-4-17.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSLock_4.17Tests/NSLock_4_17Tests.m
@@ -0,0 +1,34 @@
+//
+//  NSLock_4_17Tests.m
+//  NSLock_4.17Tests
+//
+//  Created by qianfeng on 14-4-17.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSLock_4_17Tests : XCTestCase
+
+@end
+
+@implementation NSLock_4_17Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
